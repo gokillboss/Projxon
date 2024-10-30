74e00175038f64c8bebdb4890bb6e815be29a908
Brandon Diep <brandondiep2711@gmail.com>
2024-10-30 16:08:50 -0700

    added axios instance in utils as import to blogService

--- projxon-web/src/services/blogService.js
+++ projxon-web/src/services/blogService.js
@@ -1,12 +1,12 @@
 
-import axios from 'axios';
+import axiosInstance from '../utils/axiosInstance'
 
 export const fetchBlogs = async () => {
-  const response = await axios.get('/api/blogs');
+  const response = await axiosInstance.get('/api/blogs');
   return response.data;
 };
 
 export const fetchBlog = async (id) => {
-  const response = await axios.get(`/api/blogs/${id}`);
+  const response = await axiosInstance.get(`/api/blogs/${id}`);
   return response.data[0];
 };
