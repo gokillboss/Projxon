3c15e094dbfed82dca12eaad89692ac31ec24253
tyla <gokillboss@yahoo.com>
2024-11-07 19:44:46 -0800

    latest merge

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 
 const express = require("express");
 const path = require("path");
-const blogRoutes = require("./routes/blogs");
+const cors = require('cors')
 
 // express app
 const app = express();
 
 // Enable CORS for client origin only
-const cors = require('cors')
+
 const corsOptions = {
-   origin : ['https://testprojxon.onrender.com', 'https://gokillboss.github.io/Projxon', 'https://localhost:3000'],
+    origin: ['https://testprojxon.onrender.com', 'https://gokillboss.github.io/Projxon', 'https://localhost:3000'],
 }
 app.use(cors(corsOptions))
 
@@ -18,32 +18,36 @@ app.use(cors(corsOptions))
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
+    console.log(req.path, req.method)
+    next()
 })
 
 // Render React as View
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // Setup Routes For Which The Server Is Listening
-app.use('/api/blogs', blogRoutes)
+const routes = require('./routes')
+app.use('/api/', routes)
 
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+    res.status(404).json({ message: "Not found" });
 });
+
+
 
 // error handler
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error", stack } = err;
-  console.log(stack);
-  res.status(status).json({ message });
+    const { status = 500, message = "Server error", stack } = err;
+    console.log(stack);
+    res.status(status).json({ message });
 });
 
 
 // Server Running
-app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
