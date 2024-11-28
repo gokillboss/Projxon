2f7fcd323050dc61aecea754d4a6e2c0284742c5
Brandon Diep <brandondiep2711@gmail.com>
2024-11-27 16:05:11 -0800

    fixed getting all blogs bug, added 'Load More' button to get more blogs

--- projxon-web/src/pages/ResearchPage.js
+++ projxon-web/src/pages/ResearchPage.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react"
 
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import { LuBookOpen , LuCalendar, LuFileText, LuMail } from "react-icons/lu";
+import { Container, Button } from 'react-bootstrap';
+// import { LuBookOpen , LuCalendar, LuFileText, LuMail } from "react-icons/lu";
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 import Hero from '../components/Hero';
-import InfoForm from '../components/InfoForm';
+import CallToAction from '../components/CallToAction';
 import BlogCard from '../components/BlogCard';
+import LoadingSpinner from '../components/LoadingSpinner';
+
 
 import { fetchBlogs } from '../services/blogService'
 
@@ -17,48 +19,60 @@ import './ResearchPage.css';
 
 const ResearchPage = () => {
 
-    const cards = [
-        {
-            icon: <LuBookOpen size={23} />,
-            title: 'Blog',
-            description: 'Stay Updated with the Latest News, Events, and Insights.',
-            action: 'Read More'
-        },
-        {
-            icon: <LuCalendar size={23} />,
-            title: 'Events',
-            description: 'Join our upcoming events and webinars to stay ahead in the industry.',
-            action: 'View Events'
-        },
-        {
-            icon: <LuFileText size={23} />,
-            title: 'Articles',
-            description: 'Read our detailed articles and case studies on various topics.',
-            action: 'Explore Articles'
-        },
-        {
-            icon: <LuMail size={23} />,
-            title: 'Newsletter',
-            description: 'Subscribe to our newsletter for the latest updates and exclusive content.',
-            action: 'Subscribe'
-        },
-    ]
+    // const cards = [
+    //     {
+    //         icon: <LuBookOpen size={23} />,
+    //         title: 'Blog',
+    //         description: 'Stay Updated with the Latest News, Events, and Insights.',
+    //         action: 'Read More'
+    //     },
+    //     {
+    //         icon: <LuCalendar size={23} />,
+    //         title: 'Events',
+    //         description: 'Join our upcoming events and webinars to stay ahead in the industry.',
+    //         action: 'View Events'
+    //     },
+    //     {
+    //         icon: <LuFileText size={23} />,
+    //         title: 'Articles',
+    //         description: 'Read our detailed articles and case studies on various topics.',
+    //         action: 'Explore Articles'
+    //     },
+    //     {
+    //         icon: <LuMail size={23} />,
+    //         title: 'Newsletter',
+    //         description: 'Subscribe to our newsletter for the latest updates and exclusive content.',
+    //         action: 'Subscribe'
+    //     },
+    // ]
 
     const [blogs, setBlogs] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [visibleBlogs, setVisibleBlogs] = useState(6)
+
+    const handleLoadMore = () => {
+        setVisibleBlogs((prev) => prev + 6)
+    }
 
     useEffect(() => {
-        AOS.init({ duration: 1200 })
+        AOS.init({ duration: 1000, once: true })
         const loadBlogs = async () => {
             try{
                 const response = await fetchBlogs()
                 setBlogs(response)
+                console.log(response)
             }
             catch(error){
                 console.log(error)
             }
+            finally{
+                setLoading(false)
+            }
         }
         loadBlogs()
     }, [])
+
+    
 
 
     return (
@@ -68,7 +82,7 @@ const ResearchPage = () => {
 
 
             {/* Research Content Section */}
-            <Container className="my-5">
+            {/* <Container className="my-5">
                 <Row className="g-4">
                     {cards.map((card, index) => (
                         <Col key={index} lg={3} md={6} sm={12}>
@@ -88,25 +102,46 @@ const ResearchPage = () => {
                         </Col>
                     ))}
                 </Row>
-            </Container>
+            </Container> */}
             
 
             {/* Recent Blogs Section */}
-            <Container className="my-5 container">
-                <h2 className='mb-3'>Recent Posts</h2>
-                <ul className="list-unstyled row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                    {blogs && blogs.map((blog, index) => (    
-                         <BlogCard blog={blog} key={index}/>                    
-                    ))}
-                </ul> 
-            </Container>
 
+                <section className="sections-container blog-section">
+                    <Container className="container">
+                        <h2 className='mb-5'>
+                            Recent Posts                         
+                            <span className="blog-heading-border mt-2"></span>
+                        </h2>
 
-            {/* Footer CTA Section */}
-            <div className="research-footer-cta text-center py-5">
-
-                <InfoForm />
-            </div>
+                        { loading ? (
+                            <div className="text-center my-5">
+                                <LoadingSpinner />
+                            </div>
+                        ) : blogs.length > 0 ? (
+                            <>
+                                <ul className="list-unstyled row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                                    {blogs && blogs.slice(0, visibleBlogs).map((blog, index) => (    
+                                        <BlogCard blog={blog} key={index}/>                    
+                                    ))}
+                                </ul> 
+                                {visibleBlogs < blogs.length && (
+                                    <div className="text-center mt-4">
+                                        <Button onClick={handleLoadMore} className="fs-5 px-4 black-button">
+                                            Load More
+                                        </Button>
+                                    </div>
+                                )}
+                            </>
+                            ) : (
+                                <div className="text-center my-5">
+                                    <p>No blog posts at the moment.</p>
+                                </div>
+                            )}
+                    </Container>
+                </section>
+            {/* Call to Action Section */}
+            <CallToAction />
         </>
     );
 };
