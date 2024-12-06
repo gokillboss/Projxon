fde8aa34a366f3efdf1211cf3874b0dfb8ac7a27
Brandon Diep <brandondiep2711@gmail.com>
2024-12-06 14:02:53 -0800

    small margin updates for when smaller screen size

--- projxon-web/src/pages/ContactPage.js
+++ projxon-web/src/pages/ContactPage.js
@@ -16,39 +16,43 @@ const ContactPage = () => {
     return (
         <div className='contact-page'>
             <Hero title="Contact Us" />
+            <section className='sections-container'>
+                <Container className="contact-section d-flex justify-content-center">
+                    <Row className='align-items-center justify-content-between'>
 
-            <Container className="contact-section d-flex justify-content-center">
-                <Row className='align-items-center justify-content-between '>
+                        <Col xs={12} md={6} className="pic" data-aos="fade-up">
+                            <Row className="d-flex flex-column align-items-center text-center gy-3">
+                                <Col>
+                                    <div className="content-container ">
 
-                    <Col xs={12} md={6} className="pic" data-aos="fade-up" data-aos-delay="200">
-                        <Row className="d-flex flex-column align-items-center text-center">
-                            <Col className='my-3'>
-                                <div className="content-container ">
+                                        <h2>At <strong>PROJXON</strong>, we're here to help.</h2>
+                                        <p className='text-begin'>
+                                            Whether you have inquiries about our <strong>services</strong>, wish to explore <strong>partnership</strong> opportunities, or are ready to embark on a journey of growth and success, our team is eager to connect with you. <strong>Let’s Transform Chaos Into Opportunity together.</strong>
+                                        </p>
+                                        <p>
+                                            <strong>Contact us</strong> and let's begin a conversation that can elevate your brand to new heights.
+                                        </p>
+                                    </div>
+                                </Col>
+                                <Col>
+                                    <div className="image-container">
+                                        <img src={contactPic} alt="Promotional" className="img-fluid" />
+                                    </div>
+                                </Col>
 
-                                    <h2>At <strong>PROJXON</strong>, we’re here to help.</h2>
-                                    <p className='text-begin'>
-                                        Whether you have inquiries about our <strong>services</strong>, wish to explore <strong>partnership</strong> opportunities, or are ready to embark on a journey of growth and success, our team is eager to connect with you. <strong>Let’s Transform Chaos Into Opportunity together.</strong>
-                                    </p>
-                                    <p>
-                                        <strong>Contact us</strong> and let’s begin a conversation that can elevate your brand to new heights.
-                                    </p>
-                                </div>
-                            </Col>
-                            <Col>
-                                <div className="image-container">
-                                    <img src={contactPic} alt="Promotional" className="img-fluid" />
-                                </div>
-                            </Col>
+                            </Row>
 
-                        </Row>
+                        </Col>
+                        
+                        <Col xs={12} md={6} className="info-form" data-aos="fade-up">
+                            <InfoForm />
+                        </Col>
+                    </Row>
 
-                    </Col>
-                    <Col xs={12} md={6} className="info-form " data-aos="fade-up" data-aos-delay="300">
-                        <InfoForm />
-                    </Col>
-                </Row>
+                </Container>
 
-            </Container>
+            </section>
+            
         </div>
     );
 };
