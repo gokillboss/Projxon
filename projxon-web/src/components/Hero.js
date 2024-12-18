aa40157943553cc2e8742115ed62fef906d97761
Brandon Diep <brandondiep2711@gmail.com>
2024-12-18 12:23:04 -0800

    test fade-in for hero

--- projxon-web/src/components/Hero.js
+++ projxon-web/src/components/Hero.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 const Hero = ({ title, subtitle, backgroundClass }) => {
   return (
-    <div className={`hero-section`} id={backgroundClass}>
+    <div className={`hero-section bg-black`} id={backgroundClass} data-aos="fade-in">
         <Container className="text-center hero-content">
           <div data-aos="fade-up">
             <h1 className="hero-title text-uppercase">{title}</h1>
